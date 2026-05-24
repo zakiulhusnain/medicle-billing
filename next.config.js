d0bfded02972d8749b375193063f5898dd04e52d
zakiulhusnain <zakiulhusnain37405@gmail.com>
2026-05-24 19:09:13 +0500

    Updated images.domains to remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com', // Yahan wo domain likhein jahan se aap images load kar rahe hain
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 }
+
 module.exports = nextConfig
